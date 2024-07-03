5575a626d886cab26631d80b09b09e46a5d2a0f7
Thin1er <vad-npk@yandex.ru>
2024-07-03 20:47:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+
+#1st program
+print(9 ** 0.5 * 5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print((1234 // 53) + (5678 // 84))
+
+#4th program
+print(int(13.42) == int((42.13-42)*100))
+
+
