9cd1facda60e94f113d409d7eadf824b9519b63f
Thin1er <vad-npk@yandex.ru>
2024-07-16 23:19:25 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,58 +1,16 @@
-my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 true_num = 0
 
-while True:
-    if my_list[0] > true_num:
-        print(my_list[0])
-    elif my_list[0] < true_num:
+
+while true_num < len(my_list):
+    num = my_list[true_num]
+    true_num = true_num + 1
+    if num == 0:
+        continue
+    elif num < 0:
         break
-    if my_list[1] > true_num:
-        print(my_list[1])
-    elif my_list[1] < true_num:
-        break
-    if my_list[2] > true_num:
-        print(my_list[2])
-    elif my_list[2] < true_num:
-        break
-    if my_list[3] > true_num:
-        print(my_list[3])
-    elif my_list[3] < true_num:
-        break
-    if my_list[4] > true_num:
-        print(my_list[4])
-    elif my_list[4] < true_num:
-        break
-    if my_list[5] > true_num:
-        print(my_list[5])
-    elif my_list[5] < true_num:
-        break
-    if my_list[6] > true_num:
-        print(my_list[6])
-    elif my_list[6] < true_num:
-        break
-    if my_list[7] > true_num:
-        print(my_list[7])
-    elif my_list[7] < true_num:
-        break
-    if my_list[8] > true_num:
-        print(my_list[8])
-    elif my_list[8] < true_num:
-        break
-    if my_list[9] > true_num:
-        print(my_list[9])
-    elif my_list[9] < true_num:
-        break
-    if my_list[10] > true_num:
-        print(my_list[10])
-    elif my_list[10] < true_num:
-        break
-    if my_list[11] > true_num:
-        print(my_list[11])
-    elif my_list[11] < true_num:
-        break
-    if my_list[12] > true_num:
-        print(my_list[12])
-    elif my_list[12] < true_num:
-        break
+    elif true_num == len(my_list):
+        print(num)
     else:
-        break
+        print(num)
