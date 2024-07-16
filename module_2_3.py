97a8db2770d82220620de4049c78b21b29ae1422
Thin1er <vad-npk@yandex.ru>
2024-07-16 22:51:58 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,58 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+true_num = 0
+
+while True:
+    if my_list[0] > true_num:
+        print(my_list[0])
+    elif my_list[0] < true_num:
+        break
+    if my_list[1] > true_num:
+        print(my_list[1])
+    elif my_list[1] < true_num:
+        break
+    if my_list[2] > true_num:
+        print(my_list[2])
+    elif my_list[2] < true_num:
+        break
+    if my_list[3] > true_num:
+        print(my_list[3])
+    elif my_list[3] < true_num:
+        break
+    if my_list[4] > true_num:
+        print(my_list[4])
+    elif my_list[4] < true_num:
+        break
+    if my_list[5] > true_num:
+        print(my_list[5])
+    elif my_list[5] < true_num:
+        break
+    if my_list[6] > true_num:
+        print(my_list[6])
+    elif my_list[6] < true_num:
+        break
+    if my_list[7] > true_num:
+        print(my_list[7])
+    elif my_list[7] < true_num:
+        break
+    if my_list[8] > true_num:
+        print(my_list[8])
+    elif my_list[8] < true_num:
+        break
+    if my_list[9] > true_num:
+        print(my_list[9])
+    elif my_list[9] < true_num:
+        break
+    if my_list[10] > true_num:
+        print(my_list[10])
+    elif my_list[10] < true_num:
+        break
+    if my_list[11] > true_num:
+        print(my_list[11])
+    elif my_list[11] < true_num:
+        break
+    if my_list[12] > true_num:
+        print(my_list[12])
+    elif my_list[12] < true_num:
+        break
+    else:
+        break
